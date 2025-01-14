3a1590ef5dab9b7f129b6782519108acb8517483
Diptanu Sarkar <diptanus961@gmail.com>
2025-01-14 18:40:56 +0530

    dip

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Enrollment = require("../models/Enrollment");
 const verifyToken = require("../middlewares/verifyToken");
 
